6cfe3c503825d1880f8c300df744980c8c1cf459
alvinjhson <alvinbest26@gmail.com>
2024-04-17 09:17:34 +0200

    First Commit

---
+++ UserTableView/UserTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserTableViewApp.swift
+//  UserTableView
+//
+//  Created by Alvin Johansson on 2024-04-17.
+//
+
+import SwiftUI
+
+@main
+struct UserTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
