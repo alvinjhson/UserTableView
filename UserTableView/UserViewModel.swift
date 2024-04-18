b89961360fdd700fac644e8e0e9497d777f9199a
alvinjhson <alvinbest26@gmail.com>
2024-04-18 14:43:02 +0200

    you can now add and change picture

--- UserTableView/UserViewModel.swift
+++ UserTableView/UserViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 class UserViewModel : ObservableObject {
  @Published var userEntries = [UserInformation]()
@@ -16,15 +17,17 @@ class UserViewModel : ObservableObject {
     }
     
     func addMockData() {
-        userEntries.append(UserInformation(name: "Alvin Johnsson", email:  "Alda@gmail.com"))
-        userEntries.append(UserInformation(name: "Eric Jöran", email:  "Jöran@gmail.com"))
-        userEntries.append(UserInformation(name: "Sven Sten", email:  "StenSven@gmail.com"))
-   
+        let defaultImage = UIImage(named: "defaultImage")! // Ersätt "standardBild" med namnet på din bild i assets
+        userEntries.append(UserInformation(name: "Alvin Johnsson", email: "Alvin@gmail.com", image: defaultImage))
+        userEntries.append(UserInformation(name: "Eric Jöran", email: "Jöran@gmail.com", image: defaultImage))
+        userEntries.append(UserInformation(name: "Sven Sten", email: "StenSven@gmail.com", image: defaultImage))
     }
-    func update(entry : UserInformation, with name: String,with email: String) {
+
+    func update(entry : UserInformation, with name: String,with email: String,with image: UIImage) {
         if let index = userEntries.firstIndex(of: entry){
             userEntries[index].name = name
             userEntries[index].email = email
+            userEntries[index].image = image
         }
         
     }
